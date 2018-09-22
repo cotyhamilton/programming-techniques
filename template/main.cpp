179a8cc72a6d3861a3f0e01079ae766f7da4f880
Coty Hamilton <cotyhamilton@gmail.com>
2018-09-21 20:32:14 -0500

    updates

---
+++ template/main.cpp
@@ -0,0 +1,25 @@
+// Coty Hamilton : problem name : ### : cotyhamilton
+
+/*
+
+    tricks of the trade:
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+
+    cin.tie(NULL);
+
+    return 0;
+
+}
+
+// cout << (i < end ? " " : "");
